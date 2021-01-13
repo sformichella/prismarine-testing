8250ff5be127a485b73ae3fcd3cd3d9c65dc7c4b
sformichella <sformichella@gmail.com>
2021-01-13 13:49:09 -0800

    add host, port, username settings

--- bot-script.js
+++ bot-script.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 module.exports = settings => {
   const bot =  mineflayer.createBot({
-    host: 'localhost', // optional
-    port: 25566,       // optional
+    host: settings.serverAddress, // optional
+    port: settings.serverPort,       // optional
     username: settings.username, // email and password are required only for
     password: '',          // online-mode=true servers
     version: false,                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
